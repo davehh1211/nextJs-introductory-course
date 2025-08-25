29d0c9daf587ddccfd32a255f2ee26bc82d46b57
David Henao <davehh1211@gmail.com>
2025-08-25 15:29:18 -0500

    chapter 13: error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,39 +26,51 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]; // YYYY-MM-DD format
     console.log('form data: ', { customerId, amount, status });
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
 
-  revalidatePath('dashboard/invoices');
-  redirect('/dashboard/invoices');
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        console.error('Database Error:', error);
+    }
+    revalidatePath('dashboard/invoices');
+    redirect('/dashboard/invoices');
 };
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function updateInvoice(id: string, formData: FormData) {
+
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
     const amountInCents = amount * 100;
-    await sql`
+    try {
+        await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
-    `;
+        `;
 
-  revalidatePath('dashboard/invoices');
-  redirect('/dashboard/invoices');
+    } catch (error) {
+        console.error('Database Error:', error);
+    }
+    revalidatePath('dashboard/invoices');
+    redirect('/dashboard/invoices');
 };
 
 export async function deleteInvoice(id: string) {
-  await sql`
-    DELETE FROM invoices
-    WHERE id = ${id}
-  `;
-
+    try {
+        await sql`
+            DELETE FROM invoices
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        console.error('Database Error:', error);
+    }
   revalidatePath('dashboard/invoices');
 }
